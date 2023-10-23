5dffebd27512e656fc1c1035d955699911bb58d4
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-10-23 23:26:45 +0100

    updated

--- starter/main.js
+++ starter/main.js
@@ -1,4 +1,3 @@
-
 //ES5
 
 // console.log ("send me somthing good")
@@ -9,14 +8,23 @@
 // }
 // console.log(hamburgerdiv)
 
-
-var hamburgerdiv = document.querySelector(".hamburger")
-var mobileLinks = document.querySelector(".mobile-links-holder")
-var mybackdrop = document.querySelector(".backdrop")
-
+var hamburgerdiv = document.querySelector(".hamburger");
+var mobileLinks = document.querySelector(".mobile-links-holder");
+var mybackdrop = document.querySelector(".backdrop");
 
 function dosomething() {
-    hamburgerdiv.classList.toggle("showburger")
-    mobileLinks.classList.toggle("show-mobile-links-holder")
-    mybackdrop.classList.toggle("show-backdrop")
+  hamburgerdiv.classList.toggle("showburger");
+  mobileLinks.classList.toggle("show-mobile-links-holder");
+  mybackdrop.classList.toggle("show-backdrop");
 }
+
+let sub = document.querySelector(".subscribe");
+let input = document.getElementById("subInput");
+sub.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input.value === "") {
+    alert("Entry your email");
+  } else if (input.value && "@") {
+    alert("Thank you for Subscribing");
+  }
+});
